88fd656a8c0b05728a8a20a9c61f3581d3227155
uyendang <danghuynhthuuyen@gmail.com>
2016-12-01 08:33:15 -0800

    Add files via upload

---
+++ Encode.h
@@ -0,0 +1,7 @@
+#pragma once
+//
+// Created by Luke Gilsinger, Uyen Dang, Jeremy Culbreath
+// CS 303
+// Project 3B
+// 12/2/16.
+//
---
+++ EncodeMessage.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Luke Gilsinger, Uyen Dang, Jeremy Culbreath
+// CS 303
+// Project 3B
+// 12/2/16.
+//
+#include <iostream>
+#include "Decode.h"
+#include "Encode.h"
+
+using namespace std;
