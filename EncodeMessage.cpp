493885dcdf4e26ddd82d32bf42531d30e976b7d3
uyendang <danghuynhthuuyen@gmail.com>
2016-12-01 08:38:35 -0800

    Add files via upload

--- EncodeMessage.cpp
+++ EncodeMessage.cpp
@@ -9,3 +9,62 @@
 #include "Encode.h"
 
 using namespace std;
+class Encode
+{
+private:
+	struct Node
+	{
+		string letter;
+		string code;
+		Node *left;
+		Node *right;
+	};
+	Node *root;
+
+public:
+	Encode()
+	{
+		root = NULL;
+	}
+	void Insert(Node *&r, string letter, string code)
+	{
+		if (r == NULL)
+		{
+			r = new Node;
+			r->letter = letter;
+			r->code = code;
+			r->left = r->right = NULL;
+		}
+	}
+	void Insert(string letter, string code)
+	{
+		Node *r = root;
+		if (r != NULL)
+		{
+			for (int i = 0; i < code.length(); i++)
+			{
+				if (code[i] == '.')
+				{
+					if (r->left)
+						r = r->left;
+					else
+					{
+						// todo: code to bind a new node to r->left
+						break;
+					}
+				}
+				else // its always either '.' or '-' so there is no need to double check the symbol here
+				{
+					if (r->right)
+						r = r->right;
+					else
+					{
+						// todo: code to bind a new node to r->right
+						break;
+					}
+				}
+			}
+		}
+		else Insert(root, letter, code);
+	}
+};
